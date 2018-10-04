0353ac8ac699ee30feaa107da75fe71292122525
POA-INFO Jairo Conceicao <Jairo.POA@penagro.mocrusul>
2018-10-04 18:38:25 -0300

    Initial Project

---
+++ src/views/Vanilla.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <fieldset>
+      <section class="row">
+        <div class="col-4">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="date" id="exampleInputEmail1" placeholder="Email">
+        </div>
+
+        <div class="col-4">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" id="exampleInputPassword1" placeholder="Password">
+        </div>
+
+        <div class="col-4">
+          <label for="exampleInputFile">File input</label>
+          <input type="file" id="exampleInputFile">
+        </div>
+      </section>
+
+      <section class="row">
+        <div class="col-12">
+          <input type="checkbox" id="CheckMe">
+          <label for="CheckMe">Check me out</label>
+        </div>
+      </section>
+      
+      <section class="row">
+        <div class="col-12">
+          <button type="submit">Submit</button>
+        </div>
+      </section>
+    </fieldset>
+  </form>
+</template>
